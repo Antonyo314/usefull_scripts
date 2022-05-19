8fcbaff2b54c0c4fa173049c2a770c58e8c416fe
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-05-19 15:41:32 +0300

    refactoring

--- download_bucket_from_aws_s3.py
+++ download_bucket_from_aws_s3.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-zip_name = 'leftImg8bit.zip'
+zip_name = 'gtFine_trainvaltest.zip'
 
 session = boto3.Session(
     aws_access_key_id='',
@@ -12,4 +12,4 @@ s3 = session.resource('s3')
 
 bucket = s3.Bucket('latentai-ai-ml')
 os.makedirs('downloads', exist_ok=True)
-bucket.download_file(f'data/{zip_name}', f'/Users/anton/PycharmProjects/latentai-application-framework/downloads/{zip_name}')
+bucket.download_file(f'data/{zip_name}', f'downloads/{zip_name}')
